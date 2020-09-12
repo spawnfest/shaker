3e31941667f263878a8c8568c3f6bd7b92ab9b8f
hissssst <csqcsqcsq@yandex.ru>
2020-09-12 22:40:08 +0300

    Added model implementation

--- lib/shaker/generator/function.ex
+++ lib/shaker/generator/function.ex
@@ -1,23 +1,26 @@
 defmodule Shaker.Generator.Function do
 
   @moduledoc """
-  Generates quoted def-function which returns keyword
+  Generates quoted def-functions
   """
 
-  @spec gen_one(atom(), Keyword.t()) :: Macro.t()
-  def gen_one(name, keyword) do
-    gen_func(name, [], keyword)
+  @type option :: {atom(), boolean()}
+  @type options :: [option()]
+
+  @spec gen_one(atom(), any(), options()) :: Macro.t()
+  def gen_one(name, body, opts \\ []) do
+    gen_func(name, [], body, opts)
   end
 
-  @spec gen_clauses(atom(), Keyword.t(Keyword.t())) :: Macro.t()
-  def gen_clauses(name, clauses) do
+  @spec gen_clauses(atom(), Keyword.t(), options()) :: Macro.t()
+  def gen_clauses(name, clauses, opts \\ []) do
     Enum.map(clauses, fn {arg, code} ->
-      gen_func(name, [arg], code)
+      gen_func(name, [arg], code, opts)
     end)
   end
 
-  defp gen_func(name, args, body) do
-    body = Macro.escape(body)
+  defp gen_func(name, args, body, opts) do
+    body = if(not !!opts[:no_escape], do: Macro.escape(body), else: body)
     quote do
       def unquote(name)(unquote_splicing(args)) do
         unquote(body)
--- lib/shaker/generator/mix.ex
+++ lib/shaker/generator/mix.ex
@@ -4,10 +4,63 @@ defmodule Shaker.Generator.Mix do
   View for generating mix.exs file from prepared structure
   """
 
-  alias Shaker.Generator.Function
+  @maxlen 80 - 2 - 2 - 2 - 3
 
-  @spec gen_module(atom(), [Macro.t()]) :: Macro.t()
-  defp gen_module(name, functions) do
+  alias Shaker.Generator.Function
+  alias Shaker.Model.Mix, as: Model
+
+  @spec gen(atom(), Model.t()) :: Macro.t()
+  def gen(name, %{application: app, project: proj}) do
+    [
+      gen_funcs(:application, app),
+      gen_funcs(:project, proj)
+    ]
+    |> List.flatten()
+    |> gen_module(name)
+  end
+
+  defp gen_funcs(name, keyword) do
+    {values, funcs} =
+      Enum.reduce(keyword, {[], []}, fn {key, value}, {values, funcs} ->
+        case gen_func_or_value(key, value) do
+          {:value, val} ->
+            {[{key, val} | values], funcs}
+
+          {:func, fun} ->
+            {[{key, quote(do: unquote(key)())} | values], [fun | funcs]}
+
+          {:env_funcs, env_funcs} ->
+            {
+              [{key, quote(do: unquote(key)(Mix.env()))} | values],
+              env_funcs ++ funcs
+            }
+        end
+      end)
+
+    [Function.gen_one(name, values, no_escape: true) | funcs]
+  end
+
+  defp gen_func_or_value(key, ["$anyenv": value]) do
+    if get_kv_len(key, value) > @maxlen do
+      {:func, Function.gen_one(key, value)}
+    else
+      {:value, value}
+    end
+  end
+  defp gen_func_or_value(key, pairs) do
+    clauses =
+      case Keyword.fetch(pairs, :"$anyenv") do
+        {:ok, v} ->
+          Keyword.delete(pairs, :"$anyenv") ++ [{Macro.var(:_, nil), v}]
+
+        _ ->
+          pairs
+      end
+    {:env_funcs, Function.gen_clauses(key, clauses)}
+  end
+
+  @spec gen_module([Macro.t()], atom()) :: Macro.t()
+  defp gen_module(functions, name) do
     quote do
       defmodule unquote(name) do
         unquote_splicing(functions)
@@ -15,56 +68,8 @@ defmodule Shaker.Generator.Mix do
     end
   end
 
-  def gen(params) do
-    [
-      gen_application(params),
-      gen_project(params)
-    ]
-    |> List.flatten()
-  end
-
-  def gen_application(%{application: keyword}) do
-    [Function.gen_one(:application, filter_empty(keyword))]
-  end
-
-  def gen_project(%{project: project}) do
-    gen_flatten(:project, project, [:description, :project])
-  end
-
-  def gen_flatten(name, map_or_kw, keys) do
-    keywords = filter_empty(map_or_kw)
-    {kws, funcs} = gen_functions(keywords, keys)
-    [Function.gen_one(name, Keyword.merge(keywords, kws)) | funcs]
-  end
-
-  # Replaces passed keys in keyword with calls to these separate functions
-  @spec gen_functions(Keyword.t(), [atom()]) :: {Keyword.t(Macro.t()), [Macro.t()]}
-  defp gen_functions(keyword, keys) do
-    keyword
-    |> Keyword.take(keys)
-    |> Enum.reduce({[], []}, fn {key, keyword}, {kw, funcs} ->
-      {
-        [{key, quote(do: unquote(key)())} | kw],
-        [Function.gen_one(key, filter_empty(keyword)) | funcs]
-      }
-    end)
-  end
-
-  # Helpers
-
-  defguardp is_pairs(x) when is_map(x) or is_list(x)
-
-  @spec filter_empty(any()) :: any()
-  defp filter_empty(pairs) when is_pairs(pairs) do
-    Enum.filter(pairs, fn {_, x} -> x != [] end)
-  end
-  defp filter_empty(other), do: other
-
-  @spec to_block([Macro.t()]) :: Macro.t()
-  def to_block(quoteds) do
-    quote do
-      unquote_splicing(quoteds)
-    end
+  defp get_kv_len(key, value) do
+    String.length(Atom.to_string(key)) + String.length(inspect(value))
   end
 
 end
