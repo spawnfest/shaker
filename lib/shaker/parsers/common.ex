5cbf23fd674160880cf27762af313e8666fa6c4f
Dmitry Rubinstein <rubinsteindb@gmail.com>
2020-09-13 11:15:19 +0300

    Making rebar.config parser

--- lib/shaker/parsers/common.ex
+++ lib/shaker/parsers/common.ex
@@ -1,10 +1,8 @@
 defmodule Shaker.Parsers.Common do
-
   def read_from(root_path, wildcard) do
     case Path.wildcard(Path.join(root_path, wildcard)) do
       [path] -> :file.consult(path)
       _ -> {:error, :bad_file}
     end
   end
-
 end
